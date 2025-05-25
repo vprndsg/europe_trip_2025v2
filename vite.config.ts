4aa6ffc6ccbcd0f4b00e315b827760393b733071
vprndsg <michael.phillips.a@gmail.com>
2025-05-24 22:28:18 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+   
+    
     return {
-      define: {
+      
+            base: '/europe_trip_2025v2/',
+            define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
